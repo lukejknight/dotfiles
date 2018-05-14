b7aeb3013f31d821f12f7ee4bb1faffbaa49ec02
Luke Knight <lukeknight93@gmail.com>
2018-05-14 22:58:20 +1000

    Add bash configuration

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-ln -s ~/Projects/dotfiles/.emacs.d ~/.emacs.d
-mkdir ~/.emacs-saves
+if [ ! -d ~/.emacs-saves ]; then
+  mkdir -p ~/.emacs-saves;
+fi
+
+ln -sfh ~/Projects/dotfiles/.emacs.d ~/.emacs.d
+ln -sf ~/Projects/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/Projects/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/Projects/dotfiles/.bash_prompt ~/.bash_prompt
+
+curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
+curl -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.git-completion.bash
+
