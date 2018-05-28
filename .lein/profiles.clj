09a52dab67eef96cc15f1f3c977f85d4d0b485ba
Luke Knight <lukeknight93@gmail.com>
2018-05-28 23:21:51 +1000

    Add leiningen profiles.clj

---
+++ .lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user 
+  {:repl 
+    {:plugins [[cider/cider-nrepl "0.16.0"]]
+     :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}}
+ 
--- install.sh
+++ install.sh
@@ -5,9 +5,12 @@ if [ ! -d ~/.emacs-saves ]; then
 fi
 
 ln -sfh ~/Projects/dotfiles/.emacs.d ~/.emacs.d
+
 ln -sf ~/Projects/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/Projects/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/Projects/dotfiles/.bash_prompt ~/.bash_prompt
+
+ln -sfh ~/Projects/dotfiles/.lein ~/.lein
 
 curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
 curl -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.git-completion.bash
